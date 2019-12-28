fdf15e0111b5bbf237044ce5148ef69f6f7d6b39
Udcodes <blessingudochi@gmail.com>
2019-12-29 00:21:41 +0100

    imported Header component into App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React from "react";
+import Header from "./elements/Header";
 
-const App = () => <div>Start Here!</div>;
+const App = () => (
+  <div>
+    <Header />
+    Start Here!
+  </div>
+);
 
 export default App;
+
+// FEW THINGS TO NOTE ABOUT JSX
+//1. JSX is syntatic sugar for React.createElement
+//2. JSX is self closing. e.g <Header />
+//3. JSX defined components start with capital letter. e.g <Header></Header>
+//4 You can use JavaScript expressions inside of JSX, but you must nest them in {}.
+//5 You can nest components just as you would in pure HTML file.
+//6  Nested content can be assessed in props.children on your props object.
