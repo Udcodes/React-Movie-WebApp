16c5890609ca854f86ccc11d65abbad14fd56c12
Udcodes <blessingudochi@gmail.com>
2019-12-29 18:22:10 +0100

    added plugins, edited json & gitignore files.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from "../config";
 
 //Import Components Here
 import HeroImage from "./elements/HeroImage";
@@ -8,15 +9,23 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
-const Home = () => (
-  <React.Fragment>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThumb />
-    <Spinner />
-    <LoadMoreBtn />
-  </React.Fragment>
-);
+const Home = () => {
+  const [state, setState] = useState({ movies: [] });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  console.log(state);
+
+  return (
+    <React.Fragment>
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThumb />
+      <Spinner />
+      <LoadMoreBtn />
+    </React.Fragment>
+  );
+};
 
 export default Home;
