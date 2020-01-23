69fa82e348f23d7f7e1376d7d831a30c0270a7fc
Udcodes <blessingudochi@gmail.com>
2020-01-23 23:30:05 +0100

    more tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -67,7 +67,9 @@ const Home = () => {
         ))}
       </Grid>
       {loading && <Spinner />}
-      <LoadMoreBtn text="Load More" callback={loadMoreMovies} />
+      {currentPage < totalPages && !loading && (
+        <LoadMoreBtn text="Load More" callback={loadMoreMovies} />
+      )}
     </React.Fragment>
   );
 };
