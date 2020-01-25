be552485cf79514f3b155037e33e7a4fe662d611
Udcodes <blessingudochi@gmail.com>
2020-01-25 16:29:00 +0100

    built components to implement movie routing.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 
+import { Router } from "@reach/router";
+
 import Header from "./elements/Header";
 import Home from "./Home";
+import Movie from "./Movie";
+import NotFound from "./NotFound";
 
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 const App = () => (
   <React.Fragment>
     <Header />
+    <Router>
+      <Home path="/" />
+      <Movie path="/:movieId" />
+      <NotFound default />
+    </Router>
     <Home />
     <GlobalStyle />
   </React.Fragment>
@@ -16,7 +25,7 @@ const App = () => (
 export default App;
 
 // FEW THINGS TO NOTE ABOUT JSX
-//1. JSX is syntatic sugar for React.createElement
+//1. JSX is syntactic sugar for React.createElement
 //2. JSX is self closing. e.g <Header />
 //3. JSX defined components start with capital letter. e.g <Header></Header>
 //4 You can use JavaScript expressions inside of JSX, but you must nest them in {}.
--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import { Link } from "@reach/router";
 import RMDBLogo from "../images/reactMovie_logo.png";
 import TMDBLogo from "../images/tmdb_logo.svg";
 import { StyledHeader, StyledRMDBLogo, StyledTMDBLogo } from "../styles/StyledHeader";
@@ -6,7 +8,9 @@ import { StyledHeader, StyledRMDBLogo, StyledTMDBLogo } from "../styles/StyledHe
 const Header = () => (
   <StyledHeader>
     <div className="header-content">
-      <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+      <Link to="/">
+        <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+      </Link>
       <StyledTMDBLogo src={TMDBLogo} alt="tmdb-logo" />
     </div>
   </StyledHeader>
