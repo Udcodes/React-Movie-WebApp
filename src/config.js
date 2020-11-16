b66a139e9227ba93915b42c7e029ae52fa52de97
Udochi Op <57108069+Udcodes@users.noreply.github.com>
2020-11-16 11:41:29 +0100

    update config file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,3 @@
-// Configuration for TMDB
-// To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
-// Read more about the API here: https://developers.themoviedb.org/
-
 const API_URL = "https://api.themoviedb.org/3/";
 const API_KEY = "ef303b94f6c83dc85cd2cfe7bfa55127";
 
